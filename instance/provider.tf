eb253311fd7b9ffba0b47e51be974e89f4841ed4
rama.vugge <41023789+ramakrishna2324@users.noreply.github.com>
2020-04-03 17:03:26 -0500

    Create provider.tf

---
+++ instance/provider.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+
+  /* assume_role {
+            role_arn     = "arn:aws:iam::15xxxxxx:role/pnl-ss-allowterraform"
+        } */
+  access_key = ""
+
+  secret_key = ""
+}
