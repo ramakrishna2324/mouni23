f3d55879f98a9b052f5400606b20a24f7cf74d76
rama.vugge <41023789+ramakrishna2324@users.noreply.github.com>
2020-04-03 16:13:51 -0500

    ecs-ec2

---
+++ provider.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "eu-west-1"
+
+  /* assume_role {
+            role_arn     = "arn:aws:iam::15xxxxxx:role/pnl-ss-allowterraform"
+        } */
+  access_key = "AKIASVYEHVGAJFRC42W5"
+
+  secret_key = "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"
+}
